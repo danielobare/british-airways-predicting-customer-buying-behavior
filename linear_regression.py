131fa508637287aeb4c906afd1e9c90c9f6f7da7
Daniel Obare <dnobare@gmail.com>
2023-07-04 03:56:29 +0300

    Added load_wine dataset
    Important files are load_wine that I used to get started in learning about scikit-learn

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,4 @@
 from sklearn import linear_model
 reg = linear_model.LinearRegression()
-reg.fit([[0,0], [1,1], [2,2]], [0,1,2])
-LinearRegression()
+reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
 reg.coef_
-array([0.5, 0.5])
